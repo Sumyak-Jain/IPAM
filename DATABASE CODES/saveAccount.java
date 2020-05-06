c3c766962cca8f843cff05020af13b64a7c99aa6
SUMYAK JAIN <3456gaurav@gmail.com>
2020-05-07 02:40:11 +0530

    Create saveAccount.java

---
+++ DATABASE CODES/saveAccount.java
@@ -0,0 +1,30 @@
+private void saveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveActionPerformed
+       String url = "jdbc:mysql://localhost:3306/ipam";
+        String user = "root";
+        String password = "01234";
+ 
+         String name=username_txt.getText();
+        String passwd=password_txt.getText();
+        
+ 
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+ 
+            String sql = "insert into login (username,password)" + " values (?,?)";
+            PreparedStatement statement = conn.prepareStatement(sql);
+ 
+            statement.setString(1, name);
+              statement.setString(2, passwd);
+              
+ 
+            int row = statement.executeUpdate();
+            if (row > 0) {
+               
+                JOptionPane.showMessageDialog(null,"Account Created");
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+      
+    }
