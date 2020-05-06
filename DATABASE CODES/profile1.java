3cf6a8b102a20ba96e70e8d3a3047b2ee75c1202
SUMYAK JAIN <3456gaurav@gmail.com>
2020-05-07 02:03:43 +0530

    Create profile1.java

---
+++ DATABASE CODES/profile1.java
@@ -0,0 +1,67 @@
+    //this is the code to extract data from database and display it on the textfield according to the button clicked
+    
+    private void profile1ActionPerformed(java.awt.event.ActionEvent evt) {
+       String url = "jdbc:mysql://localhost:3306/ipam";
+        String user = "root";
+        String password = "01234";
+ 
+         String Pname=profile1.getText();
+        System.out.println(Pname);
+        
+ 
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+ 
+          String SQL="Select profile_name,ip,subnet,gateway,dns1,dns2 from profile where profile_name='"+Pname+"'";
+          
+           PreparedStatement statement = conn.prepareStatement(SQL);
+ 
+          // statement.setString(1,Pname);
+
+   
+    
+
+    ResultSet rs=statement.executeQuery(SQL);
+    if(rs.next()==false){
+        Window3 obj=new Window3();
+        this.setVisible(false);
+        obj.setVisible(true);
+    }else{
+    do
+
+    {
+    String pn= rs.getString("profile_name");
+    String ip= rs.getString("ip");
+    String sn= rs.getString("subnet");
+    String gw= rs.getString("gateway");
+    String dns1_= rs.getString("dns1");
+    String dns2_= rs.getString("dns2");
+    
+     Window3 obj1=new Window3();
+    this.setVisible(false);
+    obj1.setVisible(true);
+  // Window3 obj=new Window3();
+    
+    obj1.profile_txt.setText(pn);
+    
+    obj1.IP_txt.setText(ip);
+   
+    obj1.subnet_text.setText(sn);
+    
+    obj1.gateway_text.setText(gw);
+    
+    obj1.dns1_txt.setText(dns1_);
+    
+    obj1.dns2_txt.setText(dns2_);
+    
+    
+    }while(rs.next());
+    }
+    conn.close();
+    statement.close();
+  
+    
+    
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
